4a6adbe4d53863fd85a004d924dc7c9b1439405e
codeleilei <coderleilei@163.com>
2023-02-26 16:23:30 +0800

    fix: 撇弃下划线语法

--- lib/core/parseText.ts
+++ lib/core/parseText.ts
@@ -4,8 +4,8 @@ import { parseSuperLink } from "./parseSuperLink";
 export function parseNormalText(text: string, inner: boolean = false) {
   let result = processStrongText(text);
   result = processObliqueText(result);
-  result = processStrongText2(result);
-  result = processObliqueText2(result);
+  // result = processStrongText2(result);
+  // result = processObliqueText2(result);
   result = parseSingleLineCode(result);
   result = processDeleteText(result);
   result = parseImage(result);
@@ -20,48 +20,65 @@ function processStrongText(text: string) {
     result += text.slice(0, idx);
     text = text.slice(idx + 2)
     let lastIdx = text.indexOf("**");
+    if(lastIdx == -1) {
+      text = '**' + text;
+      break;
+    }
     result += `<strong>${text.slice(0, lastIdx)}</strong>`;
     text = text.slice(lastIdx + 2)
   }
   text && (result += text);
   return result;
 }
-function processStrongText2(text: string) {
-  let result = '', idx = -1;
-  while ((idx = text.indexOf("__")) != -1) {
-    result += text.slice(0, idx);
-    text = text.slice(idx + 2)
-    let lastIdx = text.indexOf("__");
-    result += `<strong>${text.slice(0, lastIdx)}</strong>`;
-    text = text.slice(lastIdx + 2)
-  }
-  text && (result += text);
-  return result;
-}
+// 暂时不用 因为它会影响包含__的字符
+// function processStrongText2(text: string) {
+//   let result = '', idx = -1;
+//   while ((idx = text.indexOf("__")) != -1) {
+//     result += text.slice(0, idx);
+//     text = text.slice(idx + 2)
+//     let lastIdx = text.indexOf("__");
+//     if(lastIdx == -1) {
+//       text = '__' + text;
+//       break;
+//     }
+//     result += `<strong>${text.slice(0, lastIdx)}</strong>`;
+//     text = text.slice(lastIdx + 2)
+//   }
+//   text && (result += text);
+//   return result;
+// }
 function processObliqueText(text: string) {
   let result = '', idx = -1;
   while ((idx = text.indexOf("*")) != -1) {
     result += text.slice(0, idx);
     text = text.slice(idx + 1)
     let lastIdx = text.indexOf("*");
+    if(lastIdx == -1) {
+      text = '*' + text;
+      break;
+    }
     result += `<i>${text.slice(0, lastIdx)}</i>`;
     text = text.slice(lastIdx + 1)
   }
   text && (result += text);
   return result;
 }
-function processObliqueText2(text: string) {
-  let result = '', idx = -1;
-  while ((idx = text.indexOf("_")) != -1) {
-    result += text.slice(0, idx);
-    text = text.slice(idx + 1)
-    let lastIdx = text.indexOf("_");
-    result += `<i>${text.slice(0, lastIdx)}</i>`;
-    text = text.slice(lastIdx + 1)
-  }
-  text && (result += text);
-  return result;
-}
+// function processObliqueText2(text: string) {
+//   let result = '', idx = -1;
+//   while ((idx = text.indexOf("_")) != -1) {
+//     result += text.slice(0, idx);
+//     text = text.slice(idx + 1)
+//     let lastIdx = text.indexOf("_");
+//     if(lastIdx == -1) {
+//       text = '_' + text;
+//       break;
+//     }
+//     result += `<i>${text.slice(0, lastIdx)}</i>`;
+//     text = text.slice(lastIdx + 1)
+//   }
+//   text && (result += text);
+//   return result;
+// }
 // 删除线
 function processDeleteText(text: string) {
   let result = '', idx = -1;
@@ -69,6 +86,10 @@ function processDeleteText(text: string) {
     result += text.slice(0, idx);
     text = text.slice(idx + 2)
     let lastIdx = text.indexOf("~~");
+    if(lastIdx == -1) {
+      text = '~~' + text;
+      break;
+    }
     result += `<del>${text.slice(0, lastIdx)}</del>`;
     text = text.slice(lastIdx + 2)
   }
@@ -82,6 +103,10 @@ function parseSingleLineCode(text: string) {
     result += text.slice(0, idx);
     text = text.slice(idx + 1)
     let lastIdx = text.indexOf("`");
+    if(lastIdx == -1) {
+      text = '`' + text;
+      break;
+    }
     result += `<code class=single-code>${text.slice(0, lastIdx)}</code>`;
     text = text.slice(lastIdx + 1)
   }
