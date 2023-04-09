1dcbb639a239bc843edaf29af137d635495b295a
codeleilei <coderleilei@163.com>
2023-04-09 22:19:58 +0800

    feat: add languages

--- lib/core/parseCode.ts
+++ lib/core/parseCode.ts
@@ -3,8 +3,25 @@ import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.min.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.min.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.min.js";
+
+// 解析的语言 预设...
+import "prismjs/components/prism-java-min.js";
+import "prismjs/components/prism-css-min.js";
+import "prismjs/components/prism-dart-min.js";
+import "prismjs/components/prism-go-min.js";
+import "prismjs/components/prism-http-min.js";
+import "prismjs/components/prism-json-min.js";
+import "prismjs/components/prism-kotlin-min.js";
+import "prismjs/components/prism-less-min.js";
+import "prismjs/components/prism-objectivec-min.js";
+import "prismjs/components/prism-php-min.js";
+import "prismjs/components/prism-python-min.js";
+import "prismjs/components/prism-ruby-min.js";
+import "prismjs/components/prism-rust-min.js";
+import "prismjs/components/prism-swift-min.js";
+import "prismjs/components/prism-sql-min.js";
+import "prismjs/components/prism-typescript-min.js";
 /**
- * 这里暂时只处理了脚本和html，后续会考虑继续增加。。(除了标记语言，所有语言都是统一进行处理的)
  * @param templates md切割后的模版
  * @param i 当前开始处理位置
  * @param templateLength 模板的总长度
@@ -34,6 +51,7 @@ export function parseCode(
     Prism.languages[language],
     language
   )}</code></pre>`;
+
   return { startIdx: i, result };
 }
 
