355342977633f302ffac8d668a7afdc9223ab4d4
codeleilei <coderleilei@163.com>
2023-04-10 13:58:53 +0800

    feat: 代码块复制&代显示

--- lib/core/parseCode.ts
+++ lib/core/parseCode.ts
@@ -3,6 +3,10 @@ import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.min.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.min.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.min.js";
+import "prismjs/plugins/toolbar/prism-toolbar.min.js";
+import "prismjs/plugins/toolbar/prism-toolbar.min.css";
+import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js";
+import "prismjs/plugins/show-language/prism-show-language.min.js";
 
 // 解析的语言 预设...
 import "prismjs/components/prism-java.min.js";
