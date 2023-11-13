6f8da896fd25da9da12b438d37252edad65155be
codeleilei <coderleilei@163.com>
2023-11-13 13:36:56 +0800

    fix: table

--- lib/core/parseTable.ts
+++ lib/core/parseTable.ts
@@ -1,26 +1,38 @@
-export function parseTable(templates: string[], i: number, templateLength: number) {
-  let result = '<table>';
+import { parseNormalText } from "./parseText";
+
+export function parseTable(
+  templates: string[],
+  i: number,
+  templateLength: number
+) {
+  let result = "<table>";
   // 处理标题
   result += processTableTHead(templates[i]);
   result += `<tbody >`;
   ++i;
   for (; i < templateLength; i++) {
-    if (templates[i].trim()[0] === '|') {
+    if (templates[i].trim()[0] === "|") {
       result += processTabletBody(templates[i]);
     } else {
       break;
     }
   }
   result += `</tbody></table>`;
+  result = result.replace(/<tr><\/tr>/, "");
   return { startIdx: i - 1, result };
 }
 
 function processTableTHead(s: string) {
-  let preIdx = -1, template = '<thead><tr>';
+  let preIdx = -1,
+    template = "<thead><tr>";
   for (let i = 0, n = s.length; i < n; i++) {
-    if (s[i] == '|' && isValidedSplitChar(s, i - 1) && isValidedSplitChar(s, i + 1)) {
+    if (
+      s[i] == "|" &&
+      isValidedSplitChar(s, i - 1) &&
+      isValidedSplitChar(s, i + 1)
+    ) {
       if (preIdx != -1) {
-        template += `<th>${s.slice(preIdx + 1, i)}</th>`
+        template += `<th>${s.slice(preIdx + 1, i)}</th>`;
       }
       preIdx = i;
     }
@@ -29,15 +41,20 @@ function processTableTHead(s: string) {
 }
 
 function processTabletBody(s: string) {
-  let preIdx = -1, template = '<tr>';
+  let preIdx = -1,
+    template = "<tr>";
   for (let i = 0, n = s.length; i < n; i++) {
-    if (s[i] == '|' && isValidedSplitChar(s, i - 1) && isValidedSplitChar(s, i + 1)) {
+    if (
+      s[i] == "|" &&
+      isValidedSplitChar(s, i - 1) &&
+      isValidedSplitChar(s, i + 1)
+    ) {
       if (preIdx != -1) {
         let cnts = s.slice(preIdx + 1, i);
-        if (cnts.trim()[0] === '-') {
+        if (cnts.trim()[0] === "-") {
           continue;
         }
-        template += `<td>${cnts}</td>`
+        template += `<td>${parseNormalText(cnts)}</td>`;
       }
       preIdx = i;
     }
@@ -46,5 +63,5 @@ function processTabletBody(s: string) {
 }
 
 function isValidedSplitChar(s: string, i: number) {
-  return s[i] === ' ' || s[i] == undefined;
+  return s[i] === " " || s[i] == undefined;
 }
